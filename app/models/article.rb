f123076d8c4fbbf0efc5048846d336aeb4ba06ec
raafa16 <raafa16@hotmail.com>
2017-12-01 16:23:57 +0600

    added highlight for search results

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,13 +6,30 @@ class Article < ActiveRecord::Base
 
   after_save :re_index
 
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english'
+      indexes :subtitle, analyzer: 'english'
+      indexes :content, analyzer: 'english'
+    end
+  end
+
   def self.search(query)
     __elasticsearch__.search(
         {
             query: {
                 multi_match: {
                     query: query,
-                    fields: ['title^10', 'text']
+                    fields: ['title^10', 'subtitle', 'content']
+                }
+            },
+            highlight: {
+                pre_tags: ['<em>'],
+                post_tags: ['</em>'],
+                fields: {
+                    title: {},
+                    subtitle: {},
+                    content: {}
                 }
             }
         }
@@ -26,4 +43,13 @@ class Article < ActiveRecord::Base
   end
 end
 
+# Delete the previous articles index in Elasticsearch
+Article.__elasticsearch__.client.indices.delete index: Article.index_name rescue nil
+
+# Create the new index with the new mapping
+Article.__elasticsearch__.client.indices.create \
+  index: Article.index_name,
+  body: { settings: Article.settings.to_hash, mappings: Article.mappings.to_hash }
+
+# Index all article records from the DB to Elasticsearch
 Article.import force: true
